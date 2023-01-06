ac47b98db31f3350b64e1c27f885637c0e79f726
josuepanzo <josuepanzo91@gmail.com>
2023-01-06 12:39:37 -0300

    If Else Conditional

---
+++ session02_conditional/ex06_if_else_conditional.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+  // IF ELSE CONDITIONAL
+  int note = 3;
+
+  // FIRST EXAMPLE
+  if (note <= 5) {
+    print("Reprovado");
+  }
+
+  // SECOND EXAMPLE
+  if (note >= 10) {
+    print("Reprovado");
+  } else {
+    print("Reprovado");
+  }
+
+  // THIRD EXAMPLE
+  if (note >= 10 && note <= 20) {
+    print("Aprovado");
+  } else if (note >= 6 && note <= 9) {
+    print("Recurso");
+  } else if (note <= 5) {
+    print("Reprovado");
+  }
+}
