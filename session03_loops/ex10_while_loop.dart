750f777872daed81ccd90b3ce5bbb51befa709a6
josuepanzo <josuepanzo91@gmail.com>
2023-01-06 11:25:46 -0300

    While Loop, How it works internally

---
+++ session03_loops/ex10_while_loop.dart
@@ -0,0 +1,31 @@
+
+void main(List<String> args) {
+  // WHILE LOOP SYNTAX
+
+  /*
+  initializer;
+
+  while (condition) {
+    PUT YOUR CODE HERE
+    increment/decrement
+  }
+
+  EXAMPLE:
+  int i = 0;
+
+  whle(i <= 3) {
+    print("Loop $i");
+    i++;
+  }
+  */
+
+  // WAP TO FIND THE EVEN NUMBERS BETWEEN 1 TO 10
+
+  int count = 0;
+
+  while(count <= 10) {
+    print("Count $count");
+    count++;
+  }
+
+}
