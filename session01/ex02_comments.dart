4b1b6c9ec4cfb6eba9f968b4a2c0a4a6dad47fb4
josuepanzo <josuepanzo91@gmail.com>
2023-01-06 07:28:32 -0300

    Write Comments in Dart

---
+++ session01/ex02_comments.dart
@@ -0,0 +1,19 @@
+
+/*
+  We use this comments for Multiply Line
+  In Dart Language
+*/
+
+void main(List<String> args) {
+  // First Operation
+  print("Hy, Welcome on my app");
+
+  // Another Operation
+  print('This license is accepted to Learn Comments in Dart Language');
+
+  // Perfoming arithematic operation
+  print('Arithematic Function 15 + 5 = ${15 + 5}');
+
+  // Print Out Boolean Value
+  print("Boolean : ${true}");
+}
