55a0af1b675bcd77c3ac3c10525e12dde9d5d53a
josuepanzo <josuepanzo91@gmail.com>
2023-01-06 12:01:38 -0300

    Do While, How it Work internally

---
+++ session03_loops/ex11_do_while_loop.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  // DO WHILE LOOP SYNTAX
+
+  /*
+  initializer;
+
+  do {
+    PUT YOUR CODE HERE
+    increment/decrement
+  } while (condition);
+
+  EXAMPLE:
+  int i = 0;
+
+  do {
+    print("Loop $i");
+    i++;
+  } whle(i <= 3);
+  */
+
+  // WAP TO FIND THE EVEN NUMBERS BETWEEN 1 TO 10
+
+  int count = 0;
+
+  do {
+    print("Count $count");
+    count++;
+  } while (count <= 10);
+}
