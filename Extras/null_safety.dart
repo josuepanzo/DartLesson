a498d9f544959eaf1884d3988de2b66a197a1ddd
josuepanzo <josuepanzo91@gmail.com>
2023-01-19 06:18:00 -0300

    Using Null Safety in Dart

---
+++ Extras/null_safety.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  // NULL SAFETY
+  // Features null safety don't allow to use variable without values
+  String name;
+
+  // To force non-nullable value in variable we use check mark ?
+
+  String? description;
+
+  print(description);
+
+  late String lastName;
+  lastName = "Panzo";
+
+  print(lastName);
+}
